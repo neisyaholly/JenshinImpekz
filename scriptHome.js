0a7e3f32d5ead16637e9d78516fea69b157284f4
neisyaholly <neisya.santoso@binus.ac.id>
2023-03-23 22:53:40 +0700

    akdkjsand

--- scriptHome.js
+++ scriptHome.js
@@ -1,22 +1,28 @@
-// const moreInfoMapsMondstadt = document.querySelector("#maps-mondstadt")
-// const moreInfoMapsMondstadtDetail = document.querySelector("#more-info-mondstadt")
-// const exitInfoButtonMondstadt = document.querySelector("#exit-info-mondstadt")
-
-// moreInfoMapsMondstadt.addEventListener("click", function() {
-//     // console.log(moreInfoMapsMondstadtDetail)
-//     moreInfoMapsMondstadtDetail.style.display = "block"
-// })
-
-// exitInfoButtonMondstadt.addEventListener("click", function() {
-//     moreInfoMapsMondstadtDetail.style.display = "none"
-// })
-
-const dehya = document.getElementById("deh")
-const descDehya = document.querySelector("#desc-deh")
-const mika = document.querySelector("#mik")
-dehya.addEventListener("mouseover", function() {
-    console.log(1)
+const imgDehya = document.querySelector("#img-dehya")
+const descDehya = document.querySelector("#desc-dehya")
+const imgMika = document.querySelector("#img-mika")
+const descMika = document.querySelector("#desc-mika")
+imgDehya.addEventListener("mouseover", function() {
+    // console.log(1)
     descDehya.style.display = "block"
-    mika.style.display = "none"
+    imgMika.style.opacity = "0"
+})
+
+imgDehya.addEventListener("mouseout", function() {
+    // console.log(1)
+    descDehya.style.display = "none"
+    imgMika.style.opacity = "1"
+})
+
+imgMika.addEventListener("mouseover", function() {
+    // console.log(1)
+    descMika.style.display = "block"
+    imgDehya.style.opacity = "0"
+})
+
+imgMika.addEventListener("mouseout", function() {
+    // console.log(1)
+    descMika.style.display = "none"
+    imgDehya.style.opacity = "1"
 })
 
