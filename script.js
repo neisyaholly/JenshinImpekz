18a9977ef1f9ae00e806b27d2b4ad89e1feaf2dd
neisyaholly <neisya.santoso@binus.ac.id>
2023-03-30 10:10:23 +0700

    header/navbar

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ const menuToggle = document.querySelector('#toggle-bar')
 const listMenu = document.querySelector('.list-menu')
 
 menuToggle.addEventListener('click', function(){
-    listMenu.classList.toggle('menu-active')
+    listMenu.classList.toggle('menu-aktif')
 })
